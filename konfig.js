85a204b78b88899d67c94ed937baf9ccd381908e
Chratzon <chris.atze.ca@gmail.com>
2026-09-20 23:54:33 +0200

    Update FIREBASE_KONFIG with actual configuration

--- konfig.js
+++ konfig.js
@@ -10,7 +10,14 @@
  * Bleibt der Wert null, fragt die App wie bisher im Profil danach.
  */
 
-export const FIREBASE_KONFIG = null;
+export const FIREBASE_KONFIG = {
+  apiKey: "AIzaSyDa-dNmekolZJIbuBGzoutASa50Gli8vy0",
+  authDomain: "tippspiel-oberliga.firebaseapp.com",
+  projectId: "tippspiel-oberliga",
+  storageBucket: "tippspiel-oberliga.firebasestorage.app",
+  messagingSenderId: "533994067",
+  appId: "1:533994067:web:b663efb28c8a97f38244a6"
+};
 
 /* So sieht es ausgefüllt aus – Block ersetzen und die Zeile oben löschen:
 
